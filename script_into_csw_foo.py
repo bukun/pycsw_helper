3480803eb979cc0927c281002aeeba841893778a
bukun <bukun@osgeo.cn>
2018-07-13 21:23:30 +0800

    add script demo .

---
+++ script_into_csw_foo.py
@@ -0,0 +1,70 @@
+import uuid
+from pycsw_helper.csw_helper import MRecords
+
+mrec = MRecords()
+
+out_dic = {"identifier": uuid.uuid1(),
+           "typename": '',
+           "schema": '',
+           "mdsource": '',
+           "insert_date": '',
+           "xml": "",
+           "anytext": '',
+           "language": '',
+           "type": 'Zip File',
+           "title": '',
+           "title_alternate": '',
+           "abstract": '',
+           "keywords": '',
+           "keywordstype": '',
+           "parentidentifier": '',
+           "relation": '',
+           "time_begin": '',
+           "time_end": '',
+           "topicategory": '',
+           "resourcelanguage": '',
+           "creator": '',
+           "publisher": '',
+           "contributor": '',
+           "organization": '',
+           "securityconstraints": '',
+           "accessconstraints": '',
+           "otherconstraints": '',
+           "date": '',
+           "date_revision": '',
+           "date_creation": '',
+           "date_publication": '',
+           "date_modified": '',
+           "format": '',
+           "source": '',
+           "crs": '',
+           "geodescode": '',
+           "denominator": '',
+           "distancevalue": '',
+           "distanceuom": '',
+           "wkt_geometry": '',
+           "servicetype": '',
+           "servicetypeversion": '',
+           "operation": '',
+           "couplingtype": '',
+           "operateson": '',
+           "operatesonidentifier": '',
+           "operatesoname": '',
+           "degree": '',
+           "classification": '',
+           "conditionapplyingtoaccessanduse": '',
+           "lineage": '',
+           "responsiblepartyrole": '',
+           "specificationtitle": '',
+           "specificationdate": '',
+           "specificationdatetype": '',
+           "links": ''}
+
+# out_dic['conditionapplyingtoaccessanduse'] = raw_dic['USELIMIT'.lower()]
+out_dic["organization"] = 'OSGeo China Chapter'
+# out_dic["format"] = raw_dic['FORMDES'.lower()]
+# out_dic['date_publication'] = raw_dic['PUBTIME'.lower()]
+out_dic["source"] = 'http://www.maphub.cn'
+out_dic['publisher'] = 'OSGeo China Chapter'
+
+mrec.add_or_update(out_dic, force=True)
