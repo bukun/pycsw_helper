01fcc2022c85dfc332cbe1e4226b469b5549e39a
bukun <bukun@osgeo.cn>
2018-07-13 21:16:22 +0800

    add init script.

--- csw_helper.py
+++ csw_helper.py
@@ -1,9 +1,66 @@
 # -*- coding:utf-8 -*-
 import peewee
-from cfg import pycsw_db
+
+try:
+    from cfg import pycsw_db
+except:
+    from peewee import SqliteDatabase
+
+    pycsw_db = SqliteDatabase('xx_meta.db')
+
+
+class SPATIAL_REF_SYS(peewee.Model):
+    class Meta:
+        database = pycsw_db
+
+    '''
+    srid INTEGER NOT NULL,
+    auth_name TEXT,
+    auth_srid INTEGER,
+    srtext TEXT,
+    PRIMARY KEY (srid)
+    '''
+    srid = peewee.IntegerField(null=False, primary_key=True)
+    auth_name = peewee.TextField(null=True)
+    auth_srid = peewee.IntegerField(null=True)
+    srtext = peewee.TextField(null=True)
+
+
+class GEOMETRY_COLUMNS(peewee.Model):
+    class Meta:
+        database = pycsw_db
+
+    '''
+    f_table_catalog TEXT NOT NULL,
+    f_table_schema TEXT NOT NULL,
+    f_table_name TEXT NOT NULL,
+    f_geometry_column TEXT NOT NULL,
+    geometry_type INTEGER,
+    coord_dimension INTEGER,
+    srid INTEGER NOT NULL,
+    geometry_format TEXT NOT NULL
+    '''
+    f_table_catalog = peewee.TextField(null=False)
+    f_table_schema = peewee.TextField(null=False)
+    f_table_name = peewee.TextField(null=False)
+    f_geometry_column = peewee.TextField(null=False)
+    geometry_type = peewee.IntegerField(null=True)
+    coord_dimension = peewee.IntegerField(null=True)
+    srid = peewee.IntegerField(null=False, primary_key=True)
+    geometry_format = peewee.TextField(null=False)
 
 
 class Records(peewee.Model):
+    '''
+    identifier TEXT NOT NULL,
+    typename TEXT NOT NULL,
+    schema TEXT NOT NULL,
+    mdsource TEXT NOT NULL,
+    insert_date TEXT NOT NULL,
+    xml TEXT NOT NULL,
+    anytext TEXT NOT NULL,
+    '''
+
     class Meta:
         database = pycsw_db
 
@@ -66,15 +123,9 @@ class Records(peewee.Model):
 
 
 class MRecords(object):
-    def __init__(self):
-        self.tab_app = Records
-        # try:
-        #     self.tab_app.create_table()
-        # except:
-        #     pass
 
     def delete(self, del_id):
-        entry = self.tab_app.delete().where(self.tab_app.identifier == del_id)
+        entry = Records.delete().where(Records.identifier == del_id)
         entry.execute()
         return True
 
@@ -86,13 +137,13 @@ class MRecords(object):
                 self.delete(uid)
             else:
                 return
-        self.add(uid, postdata)
+        self.add(postdata)
 
-    def add(self, uid, postdata):
+    def add(self, postdata):
         '''
         命令行更新的
         '''
-        entry = self.tab_app.create(
+        Records.create(
             identifier=postdata["identifier"],
             typename=postdata["typename"],
             schema=postdata["schema"],
@@ -150,10 +201,48 @@ class MRecords(object):
             specificationdatetype=postdata["specificationdatetype"],
             links=postdata["links"],
         )
-        return uid
+        return postdata["identifier"]
 
     def get_by_uid(self, sig):
         try:
-            return self.tab_app.get(identifier=sig)
+            return Records.get(identifier=sig)
         except:
             return False
+
+
+def init_table():
+    GEOMETRY_COLUMNS.create_table()
+    Records.create_table()
+    SPATIAL_REF_SYS.create_table()
+
+    '''
+    'public', 'public', 'records', 'wkt_geometry', 3, 2, 4326, 'WKT'
+    '''
+    if GEOMETRY_COLUMNS.select().where(GEOMETRY_COLUMNS.srid == 4326):
+        pass
+    else:
+        GEOMETRY_COLUMNS.create(
+            f_table_catalog='public',
+            f_table_schema='public',
+            f_table_name='records',
+            f_geometry_column='wkt_geometry',
+            geometry_type=3,
+            coord_dimension=2,
+            srid=4326,
+            geometry_format='WKT')
+    '''
+    4326,'EPSG',4326,
+    'GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+    '''
+    if SPATIAL_REF_SYS.select().where(SPATIAL_REF_SYS.srid == 4326):
+        pass
+    else:
+        SPATIAL_REF_SYS.create(
+            srid=4326,
+            auth_name='EPSG',
+            auth_srid=4326,
+            srtext='GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]]'
+        )
+
+
+init_table()
